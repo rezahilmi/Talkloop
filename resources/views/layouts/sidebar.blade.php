d69401caa867a9d3d9a8f1c894f0751ff9bc7578
Reza Hilmi Dafa <rezahilmidafa@gmail.com>
2025-11-14 01:15:14 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,15 +5,6 @@
                 <ul class="pb-2 space-y-2">
                     @auth
                     <li>
-                        <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-blue-100 group dark:text-gray-200 dark:hover:bg-gray-700 @if(request()->segment(1) == 'dashboard') bg-blue-100 dark:bg-gray-700 @endif">
-                            <svg class="w-6 h-6 transition duration-75 group-hover:text-blue-500 dark:text-gray-400 dark:group-hover:text-white @if(request()->segment(1) == 'dashboard') text-blue-500 @else text-gray-500 @endif" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                            </svg>
-                            <span class="ml-3" sidebar-toggle-item>Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
                         <a href="{{ route('forum') }}" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-blue-100 group dark:text-gray-200 dark:hover:bg-gray-700 @if(request()->segment(1) == 'forum') bg-blue-100 dark:bg-gray-700 @endif">
                             <svg class="w-6 h-6 transition duration-75 group-hover:text-blue-500 dark:text-gray-400 dark:group-hover:text-white @if(request()->segment(1) == 'forum') text-blue-500 @else text-gray-500 @endif" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd" />
